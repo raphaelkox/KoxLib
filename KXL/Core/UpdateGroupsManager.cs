370d5ccf2f2037a73cbe8c5d4319f534e3251b68
RaphaelKox <raphaelkox@users.noreply.github.com>
2021-12-20 22:57:22 -0300

    update groups: group startActive property, update to use 'next' group

--- KXL/Core/UpdateGroupsManager.cs
+++ KXL/Core/UpdateGroupsManager.cs
@@ -4,173 +4,182 @@ using UnityEngine;
 
 namespace KXL.Core
 {
-    using Enumerations;
-    using Interfaces;
+using Enumerations;
+using Interfaces;
 
-    public static class UpdateGroupsManager
-    {
-        private static Dictionary<UpdateGroup, bool> UpdateGroupsActive = new Dictionary<UpdateGroup, bool>() {
-            {UpdateGroup.PlayerInput, true},
-            {UpdateGroup.MenuInput, false},
-            {UpdateGroup.Default, true},
-            {UpdateGroup.World, true},
-            {UpdateGroup.Player, true},
-        };
+public enum UpdateGroup
+{
+PlayerInput,
+MenuInput,
+Default,
+World,
+Player,
+}
 
-        private static Dictionary<UpdateGroup, bool> UpdateGroupsActiveNext = new Dictionary<UpdateGroup, bool>() {
-            {UpdateGroup.PlayerInput, true},
-            {UpdateGroup.MenuInput, false},
-            {UpdateGroup.Default, true},
-            {UpdateGroup.World, true},
-            {UpdateGroup.Player, true},
-        };
+public static class UpdateGroupsManager
+{
+private static Dictionary<UpdateGroup, bool> UpdateGroupsActive = new Dictionary<UpdateGroup, bool>() {
+{UpdateGroup.PlayerInput, true},
+{UpdateGroup.MenuInput, false},
+{UpdateGroup.Default, true},
+{UpdateGroup.World, true},
+{UpdateGroup.Player, true},
+};
 
-        public static bool IsGroupActive(UpdateGroup group) {
-            return UpdateGroupsActive[group];
-        }
-        public static void SetUpdateGroupState(UpdateGroup group, bool state) {
-            UpdateGroupsActiveNext[group] = state;
-        }
+private static Dictionary<UpdateGroup, bool> UpdateGroupsActiveNext = new Dictionary<UpdateGroup, bool>() {
+{UpdateGroup.PlayerInput, true},
+{UpdateGroup.MenuInput, false},
+{UpdateGroup.Default, true},
+{UpdateGroup.World, true},
+{UpdateGroup.Player, true},
+};
 
-        public static event Action OnPlayerInputUpdate;
-        public static event Action OnMenuInputUpdate;
-        public static event Action OnDefaultUpdate;
-        public static event Action OnWorldUpdate;
-        public static event Action OnPlayerUpdate;
+public static bool IsGroupActive(UpdateGroup group) {
+return UpdateGroupsActive[group];
+}
+public static void SetUpdateGroupState(UpdateGroup group, bool state) {
+UpdateGroupsActiveNext[group] = state;
+}
 
-        public static void OnUpdate() {
-            if (UpdateGroupsActive[UpdateGroup.PlayerInput]) {
-                OnPlayerInputUpdate?.Invoke();
-            }
-            if (UpdateGroupsActive[UpdateGroup.MenuInput]) {
-                OnMenuInputUpdate?.Invoke();
-            }
-            if (UpdateGroupsActive[UpdateGroup.Default]) {
-                OnDefaultUpdate?.Invoke();
-            }
-            if (UpdateGroupsActive[UpdateGroup.World]) {
-                OnWorldUpdate?.Invoke();
-            }
-            if (UpdateGroupsActive[UpdateGroup.Player]) {
-                OnPlayerUpdate?.Invoke();
-            }
+public static event Action OnPlayerInputUpdate;
+public static event Action OnMenuInputUpdate;
+public static event Action OnDefaultUpdate;
+public static event Action OnWorldUpdate;
+public static event Action OnPlayerUpdate;
 
-            var keys = new List<UpdateGroup>(UpdateGroupsActive.Keys);
-            foreach (var key in keys) {
-                UpdateGroupsActive[key] = UpdateGroupsActiveNext[key];
-            }
-        }
+public static void OnUpdate() {
+if (UpdateGroupsActive[UpdateGroup.PlayerInput]) {
+OnPlayerInputUpdate?.Invoke();
+}
+if (UpdateGroupsActive[UpdateGroup.MenuInput]) {
+OnMenuInputUpdate?.Invoke();
+}
+if (UpdateGroupsActive[UpdateGroup.Default]) {
+OnDefaultUpdate?.Invoke();
+}
+if (UpdateGroupsActive[UpdateGroup.World]) {
+OnWorldUpdate?.Invoke();
+}
+if (UpdateGroupsActive[UpdateGroup.Player]) {
+OnPlayerUpdate?.Invoke();
+}
 
-        public static void RegisterUpdateConsumer(IUpdatable consumer, UpdateGroup group) {
-            switch (group) {
-                case UpdateGroup.PlayerInput:
-                    OnPlayerInputUpdate += consumer.OnUpdate;
-                    break;
-                case UpdateGroup.MenuInput:
-                    OnMenuInputUpdate += consumer.OnUpdate;
-                    break;
-                case UpdateGroup.Default:
-                    OnDefaultUpdate += consumer.OnUpdate;
-                    break;
-                case UpdateGroup.World:
-                    OnWorldUpdate += consumer.OnUpdate;
-                    break;
-                case UpdateGroup.Player:
-                    OnPlayerUpdate += consumer.OnUpdate;
-                    break;
-                default:
-                    break;
-            }
-        }
+var keys = new List<UpdateGroup>(UpdateGroupsActive.Keys);
+foreach (var key in keys) {
+UpdateGroupsActive[key] = UpdateGroupsActiveNext[key];
+}
+}
 
-        public static void UnregisterUpdateConsumer(IUpdatable consumer, UpdateGroup group) {
-            switch (group) {
-                case UpdateGroup.PlayerInput:
-                    OnPlayerInputUpdate -= consumer.OnUpdate;
-                    break;
-                case UpdateGroup.MenuInput:
-                    OnMenuInputUpdate -= consumer.OnUpdate;
-                    break;
-                case UpdateGroup.Default:
-                    OnDefaultUpdate -= consumer.OnUpdate;
-                    break;
-                case UpdateGroup.World:
-                    OnWorldUpdate -= consumer.OnUpdate;
-                    break;
-                case UpdateGroup.Player:
-                    OnPlayerUpdate -= consumer.OnUpdate;
-                    break;
-                default:
-                    break;
-            }
-        }
+public static void RegisterUpdateConsumer(IUpdatable consumer, UpdateGroup group) {
+switch (group) {
+case UpdateGroup.PlayerInput:
+OnPlayerInputUpdate += consumer.OnUpdate;
+break;
+case UpdateGroup.MenuInput:
+OnMenuInputUpdate += consumer.OnUpdate;
+break;
+case UpdateGroup.Default:
+OnDefaultUpdate += consumer.OnUpdate;
+break;
+case UpdateGroup.World:
+OnWorldUpdate += consumer.OnUpdate;
+break;
+case UpdateGroup.Player:
+OnPlayerUpdate += consumer.OnUpdate;
+break;
+default:
+break;
+}
+}
 
-        public static event Action OnPlayerInputLateUpdate;
-        public static event Action OnMenuInputLateUpdate;
-        public static event Action OnDefaultLateUpdate;
-        public static event Action OnWorldLateUpdate;
-        public static event Action OnPlayerLateUpdate;
+public static void UnregisterUpdateConsumer(IUpdatable consumer, UpdateGroup group) {
+switch (group) {
+case UpdateGroup.PlayerInput:
+OnPlayerInputUpdate -= consumer.OnUpdate;
+break;
+case UpdateGroup.MenuInput:
+OnMenuInputUpdate -= consumer.OnUpdate;
+break;
+case UpdateGroup.Default:
+OnDefaultUpdate -= consumer.OnUpdate;
+break;
+case UpdateGroup.World:
+OnWorldUpdate -= consumer.OnUpdate;
+break;
+case UpdateGroup.Player:
+OnPlayerUpdate -= consumer.OnUpdate;
+break;
+default:
+break;
+}
+}
 
-        public static void OnLateUpdate() {
-            if (UpdateGroupsActive[UpdateGroup.PlayerInput]) {
-                OnPlayerInputLateUpdate?.Invoke();
-            }
-            if (UpdateGroupsActive[UpdateGroup.MenuInput]) {
-                OnMenuInputLateUpdate?.Invoke();
-            }
-            if (UpdateGroupsActive[UpdateGroup.Default]) {
-                OnDefaultLateUpdate?.Invoke();
-            }
-            if (UpdateGroupsActive[UpdateGroup.World]) {
-                OnWorldLateUpdate?.Invoke();
-            }
-            if (UpdateGroupsActive[UpdateGroup.Player]) {
-                OnPlayerLateUpdate?.Invoke();
-            }
-        }
+public static event Action OnPlayerInputLateUpdate;
+public static event Action OnMenuInputLateUpdate;
+public static event Action OnDefaultLateUpdate;
+public static event Action OnWorldLateUpdate;
+public static event Action OnPlayerLateUpdate;
 
-        public static void RegisterLateUpdateConsumer(ILateUpdatable consumer, UpdateGroup group) {
-            switch (group) {
-                case UpdateGroup.PlayerInput:
-                    OnPlayerInputLateUpdate += consumer.OnLateUpdate;
-                    break;
-                case UpdateGroup.MenuInput:
-                    OnMenuInputLateUpdate += consumer.OnLateUpdate;
-                    break;
-                case UpdateGroup.Default:
-                    OnDefaultLateUpdate += consumer.OnLateUpdate;
-                    break;
-                case UpdateGroup.World:
-                    OnWorldLateUpdate += consumer.OnLateUpdate;
-                    break;
-                case UpdateGroup.Player:
-                    OnPlayerLateUpdate += consumer.OnLateUpdate;
-                    break;
-                default:
-                    break;
-            }
-        }
+public static void OnLateUpdate() {
+if (UpdateGroupsActive[UpdateGroup.PlayerInput]) {
+OnPlayerInputLateUpdate?.Invoke();
+}
+if (UpdateGroupsActive[UpdateGroup.MenuInput]) {
+OnMenuInputLateUpdate?.Invoke();
+}
+if (UpdateGroupsActive[UpdateGroup.Default]) {
+OnDefaultLateUpdate?.Invoke();
+}
+if (UpdateGroupsActive[UpdateGroup.World]) {
+OnWorldLateUpdate?.Invoke();
+}
+if (UpdateGroupsActive[UpdateGroup.Player]) {
+OnPlayerLateUpdate?.Invoke();
+}
+}
 
-        public static void UnregisterLateUpdateConsumer(ILateUpdatable consumer, UpdateGroup group) {
-            switch (group) {
-                case UpdateGroup.PlayerInput:
-                    OnPlayerInputLateUpdate -= consumer.OnLateUpdate;
-                    break;
-                case UpdateGroup.MenuInput:
-                    OnMenuInputLateUpdate -= consumer.OnLateUpdate;
-                    break;
-                case UpdateGroup.Default:
-                    OnDefaultLateUpdate -= consumer.OnLateUpdate;
-                    break;
-                case UpdateGroup.World:
-                    OnWorldLateUpdate -= consumer.OnLateUpdate;
-                    break;
-                case UpdateGroup.Player:
-                    OnPlayerLateUpdate -= consumer.OnLateUpdate;
-                    break;
-                default:
-                    break;
-            }
-        }
-    }
+public static void RegisterLateUpdateConsumer(ILateUpdatable consumer, UpdateGroup group) {
+switch (group) {
+case UpdateGroup.PlayerInput:
+OnPlayerInputLateUpdate += consumer.OnLateUpdate;
+break;
+case UpdateGroup.MenuInput:
+OnMenuInputLateUpdate += consumer.OnLateUpdate;
+break;
+case UpdateGroup.Default:
+OnDefaultLateUpdate += consumer.OnLateUpdate;
+break;
+case UpdateGroup.World:
+OnWorldLateUpdate += consumer.OnLateUpdate;
+break;
+case UpdateGroup.Player:
+OnPlayerLateUpdate += consumer.OnLateUpdate;
+break;
+default:
+break;
+}
+}
+
+public static void UnregisterLateUpdateConsumer(ILateUpdatable consumer, UpdateGroup group) {
+switch (group) {
+case UpdateGroup.PlayerInput:
+OnPlayerInputLateUpdate -= consumer.OnLateUpdate;
+break;
+case UpdateGroup.MenuInput:
+OnMenuInputLateUpdate -= consumer.OnLateUpdate;
+break;
+case UpdateGroup.Default:
+OnDefaultLateUpdate -= consumer.OnLateUpdate;
+break;
+case UpdateGroup.World:
+OnWorldLateUpdate -= consumer.OnLateUpdate;
+break;
+case UpdateGroup.Player:
+OnPlayerLateUpdate -= consumer.OnLateUpdate;
+break;
+default:
+break;
+}
+}
+}
 }
